3d18eeacb58b8f27a1f0f34ba7d29ded7c2595c1
gabrielbiscaia <biscadev@gmail.com>
2024-10-02 02:08:35 -0300

    feat: API Rest operations implemented

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   nome: string;
+
+  @IsString()
+  @IsNotEmpty()
   sexo: string;
+
+  @IsDate()
   dataNascimento: Date;
+
+  @IsNumber()
   idade: number;
 }
